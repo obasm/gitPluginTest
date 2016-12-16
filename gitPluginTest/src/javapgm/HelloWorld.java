c364df891484bf5e1693d0f01b83d285078767b1
obasm <makliobas@hotmail.com>
2016-12-16 08:17:42 -0500

    commit from But#1234 branch

--- gitPluginTest/src/javapgm/HelloWorld.java
+++ gitPluginTest/src/javapgm/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Every body!");
+		System.out.println("Hello Fit, from branch bug#1234 !!");
 	}
 }
