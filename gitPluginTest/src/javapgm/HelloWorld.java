5886aa354b7163228fb033b0cade635725cf2163
obasm <obasm@users.noreply.github.com>
2016-12-19 06:31:57 -0500

    Update HelloWorld.java

--- gitPluginTest/src/javapgm/HelloWorld.java
+++ gitPluginTest/src/javapgm/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Every body!");
-		System.out.println("Hello Fit, from branch bug#1234 !!");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 }
